aeaea1cdd38dae65c402e3086438c977d8649356
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2024-10-19 20:39:25 +0600

    starting signIn page
    add shadcn , react-form-hook,useDebounceValue and inpelemt unique user name check

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,25 +1,61 @@
 "use client";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useDebounceValue } from "usehooks-ts";
+import { useToast } from "@/hooks/use-toast";
+import { useEffect, useState } from "react";
+import { useRouter, useRouter } from "next/navigation";
+import { signUpSchema } from "@/schemas/signUpSchema";
 
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button
-        className="bg-orange-500 px-3 py-1 m-4 rounded "
-        onClick={() => signIn()}
-      >
-        Sign in
-      </button>
-    </>
-  );
-}
+import axios, { AxiosError } from "axios";
+import { ApiResponse } from "@/types/apiResponse";
+const page = () => {
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCheckigUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const debouncedUsername = useDebounceValue(username, 300);
+  const { toast } = useToast();
+  const useRouter = useRouter();
+
+  // zod impletation
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  useEffect(() => {
+    const userCheckUniqueUsername = async () => {
+      if (debouncedUsername) {
+        setIsCheckingUsername(true);
+        setUsernameMessage("");
+        try {
+          const res = await axios.get(
+            `/api/check-username?username=${debouncedUsername}`
+          );
+
+          setUsernameMessage(res?.data?.message);
+        } catch (error) {
+          const axiosError = error as AxiosError<ApiResponse>;
+          setUsernameMessage(
+            axiosError.response?.data?.message ?? "Error Checking Username"
+          );
+        } finally {
+          setIsCheckingUsername(false);
+        }
+      }
+    };
+    userCheckUniqueUsername();
+  }, [debouncedUsername]);
+
+  return <div>page</div>;
+};
+
+export default page;
